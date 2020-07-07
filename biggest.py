fa5039a316613cf016c9e38f86715e4bc0818bdb
adithyagonti <65298859+adithyagonti@users.noreply.github.com>
2020-07-08 00:08:31 +0530

    Add files via upload

---
+++ biggest.py
@@ -0,0 +1,10 @@
+x1 = int(input('enter 1st value'))
+x2 = int(input('enter 2nd value'))
+x3 = int(input('enter 3rd value'))
+if x1>x2>x3:
+    print(x1, "is biggest")
+elif x2>x3:
+    print(x2, "is biggest")
+
+else:
+    print(x3, "is biggest")
